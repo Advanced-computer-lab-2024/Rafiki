8af40047ae220c3b7acd3d2a10b85c2cbdd6ddb5
Mina-12 <161923918+Mina-12@users.noreply.github.com>
2024-10-03 18:17:38 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TouristHomePage from './pages/TouristHomePage';
+import GuideHomePage from './pages/GuideHomePage';
+import AdvHomePage from './pages/AdvHomePage'
+import GuideSignup from './pages/GuideSignup'
+import Home from './pages/Home'
+import TouristSignup from './pages/TouristSignup'
+import AdvActivityform from './pages/AdvActivityform'
+import AdvActivityform from '../frontend/src/pages/TourismManagerMuseum'
+import TourismManagerMuseum from '../frontend/src/pages/TourismManagerMuseum';
+
+
+
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/tourist-signup" element={<TouristSignup />} />
+                <Route path="/guide-signup" element={<GuideSignup />} />
+                <Route path="/tourist-homepage" element={<TouristHomePage />} />
+                <Route path="/guide-homepage" element={<GuideHomePage />} />
+                <Route path="/adv-homepage" element={<AdvHomePage />} />
+                <Route path="/adv-activityform" element={<AdvActivityform />} />
+                <Route path="/adv-tourismManagerMuseum" element={<TourismManagerMuseum />} />
+
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
